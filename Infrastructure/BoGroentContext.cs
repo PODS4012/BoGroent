e8cc14a5c2ecaa34c7e84af1a4c55437ca28b976
D- CODE <darek_sokol@hotmail.com>
2020-11-16 22:50:58 +0100

    Added Car Model and DbContext

---
+++ Infrastructure/BoGroentContext.cs
@@ -0,0 +1,19 @@
+﻿using BoGroent.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoGroent.Infrastructure
+{
+    public class BoGroentContext : DbContext
+    {
+        public BoGroentContext(DbContextOptions<BoGroentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
