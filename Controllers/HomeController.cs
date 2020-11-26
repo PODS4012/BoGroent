f3ae5a57d452320a70d2084a1256942542ab2739
D- CODE <darek_sokol@hotmail.com>
2020-11-26 22:49:56 +0100

    small correction

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BoGroent.Infrastructure;
-using BoGroent.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
